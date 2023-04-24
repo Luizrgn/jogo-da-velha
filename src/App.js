66e27110a52a44ee38d867fe60c95593e858af76
Luiz Neto <luizrgn.cg@gmail.com>
2023-04-24 10:39:57 -0300

    entendendo o fragment
    Substituimos div desnecessaria e utilizamos o fragment para melhorar sistemas de busca SEO em nossa aplicação.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {Fragment} from 'react'; // aqui importamos o fragment, melhorando nosso SEO sistemas de busca. 
 import Sobre from './Objects/Sobre';
 import Logotipo from './Objects/Logotipo';
 
 function App() {
   return (
-    <div>
+    <Fragment> // fragment é colocado no lugar da div para não sujar nosso MARKUP que pode ser inspecionado no Search html dentro de inspector.
       <Logotipo />
       <Sobre />
-    </div>
+    </Fragment>
   );
-}
+} // outra maneira de escrever e trazer isso do Fragment é sem precisar importar ele você digitar <> "algum codigo" </> e o sistema ja vai compreender que é fragment
 
 export default App;
