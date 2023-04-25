8463f30e7513992cf293236f1247e5f4fb402383
Luiz Neto <luizrgn.cg@gmail.com>
2023-04-25 10:42:30 -0300

    estilo do menu (sanduiche)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {Fragment} from 'react'; // aqui importamos o fragment, melhorando nosso SEO sistemas de busca. 
 import Sobre from './Objects/Sobre';
 import Logotipo from './Objects/Logotipo';
+import Menu from './Objects/Menu';
 
+// fragment é colocado no lugar da div para não sujar nosso MARKUP que pode ser inspecionado no Search html dentro de inspector.
 function App() {
   return (
-    <Fragment> // fragment é colocado no lugar da div para não sujar nosso MARKUP que pode ser inspecionado no Search html dentro de inspector.
+    <Fragment> 
       <Logotipo />
       <Sobre />
+      <Menu />
     </Fragment>
   );
 } // outra maneira de escrever e trazer isso do Fragment é sem precisar importar ele você digitar <> "algum codigo" </> e o sistema ja vai compreender que é fragment
