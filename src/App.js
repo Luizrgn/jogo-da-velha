9b288183c4e59272eeefb27247a2264f4d033b88
Luiz Neto <luizrgn.cg@gmail.com>
2023-04-27 12:39:17 -0300

    criei Card Object
    Apenas o jsx, ainda falta o css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, {} from 'react';
+import React, { Fragment } from 'react';
+import Card from './Objects/Card';
 import Header, {} from './Components/Header'; // diferente do tutorial do marcobruno colabcode, ele me apontou erro de sintaxe pedindo pra digitar as chaves! só assim funcionou o codigo.
+
 
 
 function App() {
   return (
+    <Fragment>
     <Header />
+    <Card />
+    </Fragment>
   );
 } 
 
