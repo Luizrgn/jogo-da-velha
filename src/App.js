ab2cc25536cbe63277b84c289695fe6afe5fffa2
Luiz Neto <luizrgn.cg@gmail.com>
2023-05-09 10:36:40 -0300

    criação do object label

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Fragment } from 'react';
-
+import './App.css';
 import Header, {} from './Components/Header'; // diferente do tutorial do marcobruno colabcode, ele me apontou erro de sintaxe pedindo pra digitar as chaves! só assim funcionou o codigo.
 import Hashtag from './Components/Hashtag';
+import Label from './Objects/Label';
 
-
-
-
-function App() {
-  return (
-    <Fragment>
+const App = () => (
+    <main className='app'>
         <Header />
         <Hashtag />
-    </Fragment>
+        <Label content="Mostrar eventos" />
+    </main>
   );
-} 
 
 export default App;
